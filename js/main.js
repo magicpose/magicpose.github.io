a1523cbf1011d44cd76aeaa91186ba74522ae6aa
weivinner@gmail.com <weivinner@gmail.com>
2017-09-21 18:58:19 +0800

    Site updated: 2017-09-21 18:58:18

--- js/main.js
+++ js/main.js
@@ -3,11 +3,21 @@
 var hasCurrent = false;
 $('#main-nav > li').each(function () {
 	var url = window.location.href;
-	if(url.toUpperCase().indexOf($(this).text().trim().toUpperCase()) != -1){
-		$(this).addClass('current-menu-item current_page_item');
-		hasCurrent = true;
-	} else {
-		$(this).removeClass('current-menu-item current_page_item');
+	//console.log($(this).data("desc"));
+	//console.log($(this).text().trim().toUpperCase());
+	/**
+	 * if(url.toUpperCase().indexOf($(this).text().trim().toUpperCase()) != -1){
+	 * 此处根据菜单内容判断,在进行语言切换后会导致菜单的激活标示无效,配合 header-post.ejs 处理
+	 */
+	if( $(this).data("desc") == "/" ){
+		
+	}else{
+		if(url.toUpperCase().indexOf($(this).data("desc").toUpperCase()) != -1){
+			$(this).addClass('current-menu-item current_page_item');
+			hasCurrent = true;
+		} else {
+			$(this).removeClass('current-menu-item current_page_item');
+		}
 	}
 });
 
